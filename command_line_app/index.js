fe27b765bac1d504688a0890b28e6eb96a079341
Humberto <hum.alex93@gmail.com>
2024-11-28 17:12:49 -0600

    Add players using array.push

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -1,5 +1,40 @@
 
+console.log("Welcome to my app")
+
 const playerOne = "Rick"
 const playerTwo = "Jesicsa"
 
-console.log("Hello, welcome to our game", playerOne)
+
+const gameState = {
+  gameName: "Our amazing guessing game",
+  copyrightYear: 2024,
+  players: [],
+
+}
+
+const addPlayer = (playerName) => {
+  gameState.players.push(playerName)
+}
+
+
+console.log("current players:", gameState.players)
+addPlayer(playerOne)
+addPlayer(playerTwo)
+console.log("Player after adding:", gameState.players)
+
+// Function name: welcomePlayer
+// I can pass a player to this function
+// Then this function should welcome the player
+// There won't be any code repeated in terms od welcoming
+
+const welcomePlayer = (playerName) => {
+  console.log("Hello, welcome to our game", playerName)
+
+}
+
+console.log("The copyright year: ",gameState.copyrightYear)
+console.log("The name of the game:", gameState.gameName)
+
+
+welcomePlayer(playerOne)
+welcomePlayer(playerTwo)
---
+++ exploring/array_push.js
@@ -0,0 +1,16 @@
+const players = []
+
+console.log("players: ", players)
+ 
+players.push(1,2,3,4)
+const returnValueOfPush = players.push(5,6,7,8)
+
+
+console.log(players)
+console.log("Return of value of push",returnValueOfPush)
+
+
+
+
+
+
