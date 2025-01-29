45b3d9c69a3a7d7d446548488bddcca45f6676c6
Humberto <hum.alex93@gmail.com>
2025-01-29 13:05:52 -0600

    Object.keys() and Object.values()

---
+++ exploring/object_keys.js
@@ -0,0 +1,13 @@
+const frenchEnglish = {
+  salut: 'hi',
+  bonjour: 'hello',
+  evident: 'obvious',
+  oui: 'yes',
+
+}
+
+const keysOfObject = Object.keys(frenchEnglish)
+console.log(keysOfObject)
+
+const valuesOfObject = Object.values(frenchEnglish)
+console.log(valuesOfObject)
