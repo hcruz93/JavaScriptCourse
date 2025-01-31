a41499f6811a3f2aa4be412b1ebb5d0eda8ed1c3
Humberto <hum.alex93@gmail.com>
2025-01-31 11:51:04 -0600

    Calculating final results steps 2

--- command_line_app/calculateFinalResults.js
+++ command_line_app/calculateFinalResults.js
@@ -2,10 +2,29 @@ const { gameState } = require("./gameState.js")
 const { gatherResults } = require("./gatherResults.js")
 
 const calculateFinalResults = () => {
+  
+  const results = {}
+
   gameState.rounds.forEach((round, index) => {
     const roundResults = gatherResults(index)
     console.log(roundResults, index)
-  })}
+
+    const playerNames = Object.keys(roundResults)
+    //[ 'Rick', 'Jessica' ]
+    playerNames.forEach((player) => {
+      // Rick or Jessica
+      if (results[player] === undefined) {
+        results[player] = roundResults[player]
+      } else {
+        results[player] = results[player] + roundResults[player]
+      }
+    })
+  })
+
+  
+  return results
+
+}
 
 module.exports = {
   calculateFinalResults,
