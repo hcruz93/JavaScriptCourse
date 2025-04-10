d43a2baba166da32ec901e12e6dbed812accc96e
Humberto <hum.alex93@gmail.com>
2025-04-10 15:52:29 -0600

    Modern import syntax

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+import express from 'express' // ES6 module syntax
+// const express = require('express')  // CommonJS syntax
 
 const app = express()
 
@@ -6,4 +7,5 @@ app.get('/hello', (request, response) => {
   response.status(404).send('Hello our first app')
 })
 
+console.log('Server is running on port 3331')
 app.listen(3331)
