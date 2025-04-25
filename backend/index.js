cfb59fa3a9b91fa0c8eb27a80db372341b76f9ce
Humberto <hum.alex93@gmail.com>
2025-04-25 08:51:50 -0600

    State Handler

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express' // ES6 module syntax
 // const express = require('express')  // CommonJS syntax
 import { helloHandler } from './handlers/hello.js'
+import { stateHandler } from './handlers/state.js'
 
 const app = express()
 
 app.get('/hello', helloHandler )
+app.get('/state', stateHandler ) // /state endpoint to get the game state
+
+// Respond with 200
 
 console.log('Server is running on port 3331')
 app.listen(3331)
