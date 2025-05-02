4141020a39ae44cba3ff93179d7eaa29756802e0
Humberto <hum.alex93@gmail.com>
2025-05-02 09:23:20 -0600

    End point to add players step 1

---
+++ backend/handlers/addPlayer.js
@@ -0,0 +1,4 @@
+export const addPlayerHandler = (request, response) => {
+  console.log(" inside addPlayerHandler")
+  console.log("Request body", request.body)
+}
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ import express from 'express' // ES6 module syntax
 import { helloHandler } from './handlers/hello.js'
 import { stateHandler } from './handlers/state.js'
 import { getPlayersHandler } from './handlers/getPlayers.js'
+import { addPlayerHandler } from './handlers/addPlayer.js'
 
 const app = express()
+
+app.use(express.json()) // Middleware to parse JSON request body
 
 app.get('/hello', helloHandler )
 app.get('/state', stateHandler ) // /state endpoint to get the game state
 app.get('/players', getPlayersHandler ) 
+app.post('/players',addPlayerHandler) // /players endpoint to add a player
 
 // Respond with 200
 
