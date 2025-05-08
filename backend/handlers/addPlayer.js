bc5ded02ae4ad5989d168a6f7f46def45f3be502
Humberto <hum.alex93@gmail.com>
2025-05-08 09:55:39 -0600

    Returning HTTP errors

--- backend/handlers/addPlayer.js
+++ backend/handlers/addPlayer.js
@@ -11,11 +11,25 @@ export const addPlayerHandler = (request, response) => {
     return
   }
 
+  const playerWithSameName = gameState.players.find((player)=>{
+
+    return player.name === request.body.name
+
+  })
+
+  if (playerWithSameName) {
+    response.status(400).send({ 
+      message: `There is already a player with the name "${request.body.name}"`
+    })
+    return
+  }
+
   gameState.players.push({
     name:request.body.name,
     points:0,
   })
 
   response.status(201).send(gameState.players)
+  return
 }
 
