31d2d0bc9514f6a6cbf160ecc65e7481281354fe
hcruz <hum.alex93@gmail>
2025-05-19 14:03:43 -0600

    Select random element of an array

---
+++ exploring/random_item.js
@@ -0,0 +1,5 @@
+const friends =  ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+const randomIndex = Math.floor(Math.random() * friends.length);
+
+console.log(`Random friend: ${friends[randomIndex]}`);
