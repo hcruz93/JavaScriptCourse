7fc9d6df34275d2ed67fb3d612f8462c3914cf69
hcruz <hum.alex93@gmail>
2025-05-21 12:54:15 -0600

    Using .filter() in our endpoint part 2

--- backend/handlers/startNewRoundWithRandomQuestion.js
+++ backend/handlers/startNewRoundWithRandomQuestion.js
@@ -2,18 +2,30 @@ import { gameState } from "../gameState/index.js"
 import { v4 as uuidv4 } from 'uuid';
 import { questions } from "../questions.js";
 
+const hasQuestionNotBeenAsked = (question) => {
+  const hasBeenAsked = gameState.rounds.find((round) => {
+    return question.id === round.questionId
+  })
+
+  return hasBeenAsked === undefined
+}
+
 export const startNewRoundWithRandomQuestion = (request, response) => {
+
+  const notYetAskedQuestions = questions.filter((question) => {
+    return hasQuestionNotBeenAsked(question)
+  })
   
-  const randomIdx =  Math.floor(Math.random() * questions.length)
+  const randomIdx =  Math.floor(Math.random() * notYetAskedQuestions.length)
 
   const newRound = {
     id: uuidv4(),
-    questionId: questions[randomIdx].id
+    questionId: notYetAskedQuestions[randomIdx].id
   }
   
   gameState.rounds.push(newRound)
   return response.status(200).send({
     round: newRound,
-    question: questions[randomIdx]
+    question: notYetAskedQuestions[randomIdx]
   })
 }
