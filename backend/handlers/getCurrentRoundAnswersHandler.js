c8709c99a731d0c944c95445f3e67e8ac6815ee3
hcruz <hum.alex93@gmail>
2025-05-30 13:53:39 -0600

    Matching question and round using .find()

--- backend/handlers/getCurrentRoundAnswersHandler.js
+++ backend/handlers/getCurrentRoundAnswersHandler.js
@@ -1,4 +1,5 @@
 import { gameState } from '../gameState/index.js'
+import { questions } from '../questions.js'
 
 export const getCurrentRoundAnswersHandler = (request, response) => {
   if (gameState.rounds.length === 0) {
@@ -7,6 +8,22 @@ export const getCurrentRoundAnswersHandler = (request, response) => {
     })
     return
   }
+
+const currentRound = gameState.rounds[gameState.rounds.length - 1]
+
+const matchingQuestion = questions.find((question)=>{
+  return question.id === currentRound.questionId
+
+})
+
+if (!matchingQuestion) {
+  response.status(500).send({
+    message: `there was no question with ID ${currentRound.questionId}`
+  })
+  return
+}
+
+console.log(matchingQuestion)
 
   response.status(200).send({
     results: [
