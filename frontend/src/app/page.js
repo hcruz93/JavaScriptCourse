f8f1847ad84e95342aea53e92559df2850b15ad7
hcruz <hum.alex93@gmail>
2025-06-19 14:36:03 -0600

    Generating a list of JSX elements with .map()

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,16 +1,27 @@
 import Link from "next/link"
 
 export default function Home() {
-  const myName = "Rick";
-  const randomNumber = Math.random(); ;
-
+  const players = [
+  	{
+      name: "Rick",
+      points: 0,
+    },
+    {
+      name: "Jenny",
+      points: 0,
+    },
+  ]
  return (
   <div>
     <h1>Guessing game</h1>
-    <p>{myName}</p>
-    <p>{randomNumber}</p>
-    <p>{"hello"[0]}</p>
     <p>Waiting for a player</p>
+    {
+      players.map((player) => {
+        return (
+          <p>{player.name}</p>
+        )
+      })
+    }
     <h2>join</h2>
     <p>Navigate to <Link href="/joinb">/join</Link> on your phone to join the game</p>
   </div>
