f7b6b612d24cd71190a09f53b13f0d5f2e8a85a6
hcruz <hum.alex93@gmail>
2025-06-19 14:46:23 -0600

    Hard-code game state for testing

--- backend/gameState/index.js
+++ backend/gameState/index.js
@@ -1,7 +1,29 @@
 export const gameState = {
-  players: [],
+  players: [
+    {
+        name: "Chandler",
+        points: 0,
+    },
+    {
+        name: "Joey",
+        points: 0,
+    },
+    {
+        name: "Ross",
+        points: 0,
+    },
+  ],
   guesses: {},
   rounds: [],
   points: {},
 }
+
+
+// TODO: Bring back, only hard-coding for testing
+// export const gameState = {
+//   players: [],
+//   guesses: {},
+//   rounds: [],
+//   points: {},
+// }
 
