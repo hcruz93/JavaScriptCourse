122e6440064469e080f0301f5f06ecf22f90cdf3
hcruz <hum.alex93@gmail>
2025-07-17 10:01:07 -0600

    Handling button presses

--- frontend/src/app/join/page.js
+++ frontend/src/app/join/page.js
@@ -1,14 +1,27 @@
 "use client"
 
-const handlePlayerNameInputChange = (event) => {
-  console.log("What I received", event)
-}
+import { useState } from "react"
+
 export default function Join() {
+
+  const [playerName,setPlayerName]= useState(undefined)
+
+  const handlePlayerNameInputChange = (event) => {
+    setPlayerName(event.target.value)
+  }
+
+  const submitPlayerName = () => {
+    console.log("Called submit player name", playerName)
+  }
+
   return(
     <div>
       <h1>Submit your player name</h1>
       <p>
         <input type="text" onChange={handlePlayerNameInputChange}/>
+      </p>
+      <p>
+        <button type="submit" onClick={submitPlayerName}> Join Game </button>
       </p>
     </div>
   )
