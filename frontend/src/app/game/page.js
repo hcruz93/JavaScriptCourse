179a1ca907e20a5b0d057661f0b42315af8dae55
hcruz <hum.alex93@gmail>
2025-08-29 07:43:38 -0600

    Starting a new round on every page render

--- frontend/src/app/game/page.js
+++ frontend/src/app/game/page.js
@@ -1,4 +1,16 @@
+"use client"
+
+import { startRoundWithRandomQuestion } from "@/requests/startRoundWithRandomQuestion.js"
+
 export default function Game() {
+  startRoundWithRandomQuestion()
+  .then((response)=>{
+    console.log("Response from starting random round", response)
+  })
+  .catch((error)=>{
+    console.error("Error starting round", error)
+  })
+
   return(
     <div>
       <h1>Question!</h1>
---
+++ frontend/src/requests/startRoundWithRandomQuestion.js
@@ -0,0 +1,8 @@
+import axios  from "axios"
+
+export const startRoundWithRandomQuestion = () => {
+   return axios({
+    method: "POST",
+    url: "http://localhost:3331/round/start/randomquestion",
+   })
+}
