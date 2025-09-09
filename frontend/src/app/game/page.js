342432af00e4b321b8609d566dcac23ebc7b46a5
hcruz <hum.alex93@gmail>
2025-09-08 23:21:43 -0600

    Add useEffect() to initiate a call on every render

--- frontend/src/app/game/page.js
+++ frontend/src/app/game/page.js
@@ -1,20 +1,27 @@
 "use client"
 
+
+import { useEffect, useState } from "react"
 import { startRoundWithRandomQuestion } from "@/requests/startRoundWithRandomQuestion.js"
 
 export default function Game() {
-  startRoundWithRandomQuestion()
-  .then((response)=>{
-    console.log("Response from starting random round", response)
-  })
-  .catch((error)=>{
-    console.error("Error starting round", error)
-  })
+  const [prompt, setPrompt] = useState(undefined)
+
+  useEffect(() => {
+    startRoundWithRandomQuestion()
+    .then((response)=>{
+      console.log("Response from starting random round", response)
+      setPrompt(response.data.question.prompt)
+    })
+    .catch((error)=>{
+      console.error("Error starting round", error)
+    })
+  },[])
 
   return(
     <div>
       <h1>Question!</h1>
-      <p className="question">The question will go in here.</p>
+      <p className="question">{prompt}</p>
     </div>
   )
 }
