0fd37a01131b084f8f59ab1e24b30d026fb47879
hcruz <hum.alex93@gmail>
2025-09-10 06:15:06 -0600

    Text input and submit guess button on client page

--- frontend/src/app/client/page.js
+++ frontend/src/app/client/page.js
@@ -1,9 +1,29 @@
 "use client"
 
+import { useState } from 'react';
+
 export default function Client() {
-  return(
+
+  const [guess, setGuess] = useState(undefined)
+
+  const handleGuess= (event) => {
+    setGuess(event.target.value)
+  }
+
+  const handleSubmitGuess = () => {
+    console.log("submitting guess:", guess);
+  }
+
+  return (
+
     <div>
       <h1>Make a guess</h1>
+      <p>
+        <input type="text" onChange={handleGuess}/>
+      </p>
+      <p>
+        <button onClickCapture={handleSubmitGuess}>Submit guess</button>
+      </p>
     </div>
   )
 }
