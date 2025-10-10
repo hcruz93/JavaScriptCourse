e2db85704cc098abeb53ff9a4104fb47dd7656b7
hcruz <hum.alex93@gmail>
2025-10-10 12:32:24 -0600

    Dynamically hide inputs on client page

--- frontend/src/app/client/page.js
+++ frontend/src/app/client/page.js
@@ -5,8 +5,8 @@ import { makeGuess } from '@/requests/makeGuess.js';
 import * as cookieCutter from "cookie-cutter"
 
 export default function Client() {
-
   const [guess, setGuess] = useState(undefined)
+  const [submitSucces, setSubmitSucces] = useState(undefined)
 
   const handleGuess= (event) => {
     setGuess(event.target.value)
@@ -30,9 +30,11 @@ export default function Client() {
     makeGuess(playerName, guessAsNumber)
     .then((response) => {
       console.log("response for submitting", response);
+      setSubmitSucces(true)
     })
     .catch((error) => {
       console.log("Errot submitting",error);
+      setSubmitSucces(false)
     })
   }
 
@@ -40,12 +42,15 @@ export default function Client() {
 
     <div>
       <h1>Make a guess</h1>
-      <p>
+      { submitSucces == true ? null :  <p>
         <input type="text" onChange={handleGuess}/>
-      </p>
-      <p>
+      </p> }
+      { submitSucces == true ? null : <p>
         <button onClickCapture={handleSubmitGuess}>Submit guess</button>
-      </p>
+      </p>  }
+      { submitSucces == true ? <p>Your guess was submitted succesfully
+      until the round finished and we´ll reload the page for you. </p> : null}
+      { submitSucces == false ? <p>An error ocurred submittig your guess</p> : null }
     </div>
   )
 }
